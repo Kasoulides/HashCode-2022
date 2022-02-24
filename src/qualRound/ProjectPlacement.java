19117801ff4521f25dbca91cf4746c6a70ea3e31
Andreas Loizides <andreas16700@icloud.com>
2022-02-24 21:27:36 +0200

    add isValid()

---
+++ src/qualRound/ProjectPlacement.java
@@ -0,0 +1,14 @@
+package qualRound;
+
+import java.util.HashMap;
+
+public class ProjectPlacement {
+    Project project;
+    Person[] people;
+
+
+    boolean isValid(){
+        HashMap<String,Boolean> personTaken;
+
+    }
+}
