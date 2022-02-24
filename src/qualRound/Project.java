38ecf52d920c26718f5ba8501c4222be254b176e
Andreas Loizides <andreas16700@icloud.com>
2022-02-24 21:18:20 +0200

    basic data types

---
+++ src/qualRound/Project.java
@@ -0,0 +1,11 @@
+package qualRound;
+
+public final class Project{
+    final String name;
+    final Skill[] skills;
+
+    public Project(String name, Skill[] skills) {
+        this.name = name;
+        this.skills = skills;
+    }
+}
